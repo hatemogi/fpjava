8b6485d5d2f78086efcf01b38d9462985e472bd9
Daehyun Kim <hatemogi@gmail.com>
2021-11-30 14:49:43 +0900

    Tuple 클래스 추가

---
+++ app/src/main/java/fpjava/chN/Tuple.java
@@ -0,0 +1,66 @@
+package fpjava.chN;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * 임의 타입 두개의 값을 담는 튜플.
+ * io.vavr.Tuple2를 참고해서 축소 구현
+ * @param <T1> 첫번째 값
+ * @param <T2> 두번째 값
+ */
+public class Tuple<T1, T2> {
+    public final T1 _1;
+    public final T2 _2;
+
+    public Tuple(T1 t1, T2 t2) {
+        this._1 = t1;
+        this._2 = t2;
+    }
+
+    public static <T1, T2> Tuple<T1, T2> of(T1 t1, T2 t2) {
+        return new Tuple<>(t1, t2);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        } else if (!(o instanceof Tuple)) {
+            return false;
+        } else {
+            final Tuple<?, ?> that = (Tuple<?, ?>)o;
+            return Objects.equals(_1, that._1) && Objects.equals(_2, that._2);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_1, _2);
+    }
+
+    /**
+     * 첫번째 값을 바꾸기.
+     */
+    public <U1> Tuple<U1, T2> map1(@Nonnull Function<T1, U1> mapper) {
+        return of(mapper.apply(_1), _2);
+    }
+
+    /**
+     * 두번째 값을 바꾸기.
+     */
+    public <U2> Tuple<T1, U2> map2(@Nonnull Function<T2, U2> mapper) {
+        return of(_1, mapper.apply(_2));
+    }
+
+    /**
+     * 두 값을 한 번에 바꾸기.
+     * @return
+     */
+    public <U1, U2> Tuple<U1, U2> map(@Nonnull Function<T1, U1> map1,
+                                      @Nonnull Function<T2, U2> map2) {
+        return of(map1.apply(_1), map2.apply(_2));
+    }
+
+}
---
+++ app/src/test/java/fpjava/chN/TupleTest.java
@@ -0,0 +1,16 @@
+package fpjava.chN;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TupleTest {
+    @Test void equalTest() {
+        assertEquals(Tuple.of("a", 2), Tuple.of("a", 2));
+        assertNotEquals(Tuple.of(1, 2), Tuple.of(2, 1));
+    }
+
+    @Test void map() {
+        Tuple<String, Integer> t = Tuple.of("Hello", 456);
+        assertEquals(t.map1(s -> "World"), Tuple.of("World", 456));
+    }
+}
