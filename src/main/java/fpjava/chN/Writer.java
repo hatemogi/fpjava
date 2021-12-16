7d04cf56c91b81bfa512208691df2a2dde42875f
Daehyun Kim <hatemogi@gmail.com>
2021-12-16 19:15:06 +0900

    Writer 모나드 인터페이스 추가.

---
+++ src/main/java/fpjava/chN/Writer.java
@@ -0,0 +1,10 @@
+package fpjava.chN;
+
+import java.util.function.Function;
+
+public interface Writer<L, A> {
+    Writer<L, A> pure(A value);
+    <B> Writer<L, B> map(Function<A, B> f);
+    <B> Writer<L, B> flatMap(Function<A, Writer<L, B>> f);
+    Tuple<L, A> run();
+}
